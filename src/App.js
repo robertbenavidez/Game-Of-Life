7d59fa88acbbb7886c6f42270bc9d846d24de87d
Robert Benavidez <murabitbenavidez@gmail.com>
2020-09-26 13:22:26 -0400

    generating grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const numRows = 50;
+const numColums = 50;
+
+const generateEmptyGrid = () => {
+	const rows = [];
+	//iterate to create rows and colums
+	for (let i = 0; i < numRows; i++) {
+		rows.push(Array.from(Array(numColums), () => 0));
+	}
+	return rows;
+};
+
 function App() {
-	return (
-		<div className="App">
-			<p>Game of Life</p>
-		</div>
-	);
+	const [ grid, setGrid ] = useState(generateEmptyGrid());
+	console.log(grid);
+	return <div>Game of Life</div>;
 }
 
 export default App;
